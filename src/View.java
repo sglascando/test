b807768c91c472471097b0c9266c9df986b38f70
shay <shayglascott@gmail.com>
2022-04-13 12:42:07 -0600

    test

---
+++ src/View.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class View {
+    JFrame frame = new JFrame();
+    frame.setSize(400, 500);
+    JLabel jLabel = new JLabel("Usage: x op y");
+    ListModel listModel = new DefaultListModel<String>();
+
+    JPanel jPanel = new JPanel();
+
+
+}
